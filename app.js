a96d92373ecd1ae5d3dbd569b9fac5b63bac8c48
kevin <kevindaniel.123@outlook.com>
2021-06-01 19:46:50 -0500

    Página simple referenciada al app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+function mifuncion(){ return "Hernandez Diaz";}
+
+const nombre={Nombre : "kevin", segundonombre: "Daniel"};
+console.log(`Mi nombre comleto es ${nombre.Nombre} ${nombre.segundonombre} ${mifuncion()}`);
