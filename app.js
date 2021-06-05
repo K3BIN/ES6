208d4f147708e728707e6a0fa9759e39fea3e0c8
K3BIN <71346566+K3BIN@users.noreply.github.com>
2021-06-05 14:49:18 -0500

    Ejercicio1
    TL, declaraciones, ciclos, repeat
    (startsWith,endsWith,includes)

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-function mifuncion(){ return "Hernandez Diaz";}
+let personas={
+	nombres:["Kevin","Gary", "Jeremias"],
+	apellidos:["Hernández D.", "Gutierres G.","Serrano T."],
+	cuentas:["55447878", "99335656", "00338855"]
+}
 
-const nombre={Nombre : "kevin", segundonombre: "Daniel"};
-console.log(`Mi nombre comleto es ${nombre.Nombre} ${nombre.segundonombre} ${mifuncion()}`);
+const espacio=10;
+
+
+for(let i=0; i<3; i++) {
+	let dif= espacio - personas.nombres[i].length;
+	console.log(`${personas.nombres[i]} tu cuenta es: ${" ".repeat(dif)} | ${personas.cuentas[i]}`);
+}
