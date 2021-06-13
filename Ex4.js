59dc74238098db914c25ffc323f6a2498261d0e9
K3BIN <71346566+K3BIN@users.noreply.github.com>
2021-06-13 15:41:43 -0500

    Juego
    Generación de enemigo completo
    *Calculando daños

--- Ex4.js
+++ Ex4.js
@@ -1,6 +1,7 @@
 
 //-----------------------------------------Jugador-----------------------------------------
-let P1={
+let Pturn=true;
+let P={
 	Nom: prompt(`Nuevo Jugador`, "VIPER"),
 	Class: prompt(`Clase`, "Hada")	//"Milicia, Caballero, Mago o Hada"
 }
@@ -10,26 +11,36 @@ function NewPlayer(nick, {clase,hp,pm,nv} = {clase:"brujo"}){	//Parámetro(s) po
 	console.log(`${"_".repeat(45)}`);
 }
 
-NewPlayer(P1.Nom ,{clase:P1.Class, hp:700, pm:900, nv:6}/**,{hp:500,pm:1000,clase:"milicia"}*/);
+NewPlayer(P.Nom ,{clase:P.Class, hp:700, pm:900, nv:6}/**,{hp:500,pm:1000,clase:"milicia"}*/);
 //-----------------------------------------Jugador-----------------------------------------
-console.log("zzz");
+//-----------------------------------------Acciones----------------------------------------
+
+//-----------------------------------------Acciones----------------------------------------
 //-----------------------------------------Enemigo-----------------------------------------
+console.log(`${"_".repeat(45)}`);
+let Eturn=true;
+const rnd=Math.floor(Math.random() * (5 - 0)) + 0;
 let Enemy={
 	CLASE:["Ogro","Espiritu", "Muerto","demonio","angel"],
 	HP:[850,350,500,1100,1300],
 	PM:[0,1000,400,1500,1500],
 	NV:[5,6,5,2,2]
 };
-let Mov={
-	Normal(daño){let Atack=daño; /** console.log(Atack);*/},
-	Especial(){Es:100}
-};
 let {CLASE:C,HP,PM,NV} = Enemy;
-let num=Math.floor(Math.random() * (5 - 0)) + 0;
-console.log(num);
+
+let Mov={
+	Normal(at,es){let Atack=at; HP[rnd]=HP[rnd]-Atack; console.log(`Vida después de ataque ${HP[rnd]}`);},
+	Especial(){let Esp=es; PM[rnd]=PM[rnd]-Esp;}
+};
+let mov= Object.create(Mov);
+Object.setPrototypeOf( mov, Mov);
+
 NewEnemy(Mov);
-function NewEnemy( ataque,...enemigo){
-	//Mov.Normal(40);
-	console.log(`${"_".repeat(45)}`);
-	console.log(`Enemigo: ${C[num]}`);
+function NewEnemy( ataque){
+	console.log(`Enemigo:${" ".repeat(11)} | ${C[rnd]}\nVida:${" ".repeat(14)} | ${HP[rnd]}\nPoder Mágico:${" ".repeat(6)} | ${PM[rnd]}\nNivel:${" ".repeat(13)} | nv ${NV[rnd]}`);
+	if(!(!(Eturn)) && HP[rnd]>0){
+		console.log(`Vida original ${HP[rnd]}`);
+		mov.Normal(70,180);
+	}
 }
+//-----------------------------------------Enemigo-----------------------------------------
