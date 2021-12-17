2054143058aa5f20677906c1dd9f4fa4eafff869
K3BIN <71346566+K3BIN@users.noreply.github.com>
2021-12-16 20:44:38 -0600

    Map

---
+++ map.js
@@ -0,0 +1,12 @@
+const datos = [
+    {nombre: "kevin", edad: 22},
+    {nombre: "daniel", edad: 21},
+    {nombre: "allan", edad: 20},
+    {nombre: "misael", edad: 19}
+]
+
+// datos.forEach(elemento =>{
+//     console.log(elemento);
+// })
+
+datos.map(elemento => {console.log(elemento.nombre);})
